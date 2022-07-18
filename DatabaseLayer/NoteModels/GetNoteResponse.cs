52ee3d3d180d07e7b9dd52c53239b257d5128510
Ganeshp007 <potdart40.gp@gmail.com>
2022-07-18 19:08:02 +0530

    Modified: Applied validation to user input in swagger and made Email Unquie using IFElse

--- DatabaseLayer/NoteModels/GetNoteResponse.cs
+++ DatabaseLayer/NoteModels/GetNoteResponse.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DatabaseLayer.NoteModels
+﻿namespace DatabaseLayer.NoteModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
     public class GetNoteResponse
     {
         public int NoteId { get; set; }
+
         public string Title { get; set; }
+
         public string Description { get; set; }
+
         public string Bgcolor { get; set; }
+
         public int UserId { get; set; }
+
         public string Firstname { get; set; }
+
         public string Lasttname { get; set; }
+
         public string Email { get; set; }
+
         public DateTime CreatedDate { get; set; }
 
     }
--- RepositoryLayer/Interface/INoteRL.cs
+++ RepositoryLayer/Interface/INoteRL.cs
@@ -1,16 +1,19 @@
-﻿using DatabaseLayer.NoteModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RepositoryLayer.Interface
+﻿namespace RepositoryLayer.Interface
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DatabaseLayer.NoteModels;
+
     public interface INoteRL
     {
         Task AddNote(int UserId,NotePostModel notePostModel);
+
         Task<List<GetNoteResponse>> GetAllNote(int UserId);
+
         Task UpdateNote(int UserId, int NoteId, UpdateNoteModel updateNoteModel);
+
         Task DeleteNote(int UserId, int NoteId);
     }
 }
